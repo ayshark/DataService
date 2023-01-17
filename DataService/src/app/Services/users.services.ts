d4dfafa044d0d4cf97a2d26aa8d0ba9507c79304
ayshark <sjaysha@gmail.com>
2023-01-17 08:29:55 +0530

    added user service

---
+++ DataService/src/app/Services/users.services.ts
@@ -0,0 +1,9 @@
+export class UserService {
+    Users = [
+        {name: 'Aysha', status: 'active'},
+        {name: 'Mike', status: 'inactive'},
+        {name: 'Abeer', status: 'active'},
+        {name: 'Alex', status: 'active'},
+        {name: 'Zach', status: 'inactive'},
+    ]
+}
--- DataService/src/app/app.component.ts
+++ DataService/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './Services/users.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DataService';
+
+  constructor (private userService: UserService) {}
+
+  users: {name: string, status: string}[] = [];
+
+  ngOnInit() {
+    this.users = this.userService.Users
+  }
+  
 }
